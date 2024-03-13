7700c26a048497dfbdd7eb65362a029e45a70552
VaibhavSharma3107 <151926858+VaibhavSharma3107@users.noreply.github.com>
2024-03-13 16:05:57 +0530

    database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DB_HOST_URI,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, //use this with development enviroment
+    })],
+
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /*============= Implementing Swagger For API Documentation START ==================*/
+  const config = new DocumentBuilder()
+    .setTitle('Syngene Dashboard App Documentation')
+    .setDescription('The Syengene API description')
+    .setVersion('1.0.0')
+    .addTag('Syengene')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  /*============= Implementing Swagger For API Documentation END ==================*/
+
+  await app.listen(process.env.SERVER_PORT);
+
 }
 bootstrap();
